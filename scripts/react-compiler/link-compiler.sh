7e9e3ff1fa8f832fbd6d327aa0f651a0195ac102
Yash Bodade <142400356+yashhhYB@users.noreply.github.com>
2025-09-26 21:59:07 +0530

    Update link-compiler.sh

--- scripts/react-compiler/link-compiler.sh
+++ scripts/react-compiler/link-compiler.sh
@@ -3,7 +3,6 @@
 #
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
-
 set -eo pipefail
 
 if [[ "$REACT_CLASS_EQUIVALENCE_TEST" == "true" ]]; then
